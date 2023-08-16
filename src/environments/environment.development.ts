ae9ef32927a864b3a120fed478775ec14d0ba2ea
abhinav kumar pathak <abhinavpathak704@gmail.com>
2023-08-16 12:30:28 +0530

    first commit

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,10 @@
+export const environment = {
+    firebaseConfig: {
+        apiKey: "AIzaSyDSwqBaOCIZtwcoo05K759h9u-7UKGsLlg",
+        authDomain: "ang-blog-10abd.firebaseapp.com",
+        projectId: "ang-blog-10abd",
+        storageBucket: "ang-blog-10abd.appspot.com",
+        messagingSenderId: "3270621912",
+        appId: "1:3270621912:web:03d5e53fcab5c55024f5ad"
+    }
+};
